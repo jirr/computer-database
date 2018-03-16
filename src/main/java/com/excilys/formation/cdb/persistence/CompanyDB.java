5a44834fbf512e6a717f8f9aa6101356fdc4d0ba
jirr <jirr@excilys.com>
2018-03-16 11:43:49 +0100

    [Add] Slf4j dependancy + Logger in CompanyDB

--- src/main/java/com/excilys/formation/cdb/persistence/CompanyDB.java
+++ src/main/java/com/excilys/formation/cdb/persistence/CompanyDB.java
@@ -4,12 +4,17 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.excilys.formation.cdb.mapper.CompanyMP;
 import com.excilys.formation.cdb.model.Company;
 
 public enum CompanyDB {
 	
 	INSTANCE;
+	
+	private final Logger logger = LoggerFactory.getLogger(CompanyDB.class);
 	
 	private String selectAllRequest = "SELECT ca.id as caId, ca.name as caName FROM company ca";
 	
@@ -31,18 +36,22 @@ public enum CompanyDB {
 	public Company selectOne (int id) {
 		Company cres = null;
 		ResultSet res = null;
+		logger.info("Connection to database opening.");
 		try (Connection conn = ConnexionManager.INSTANCE.getConn()) {
 			PreparedStatement ps = conn.prepareStatement(selectAllRequest+" WHERE ca.id = ?;");
 			ps.setInt(1, id);
 			res = ps.executeQuery();
 			if (!res.next()) {
+				logger.warn("Fail to get data with id {}.", id);
 				return null;
 			} else {
 				cres = CompanyMP.resToCompany(res);
 			}
 		} catch (SQLException e) {
+			logger.error("Unable to reach the database.");
 			e.printStackTrace();
 		}
+		logger.info("Connection to database closed.");
 		return cres;
 	}
 }
