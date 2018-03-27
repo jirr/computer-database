157d3051a775c7955950a136bd7b3e91b3fc44a5
jirr <jirr@excilys.com>
2018-03-27 17:13:21 +0200

    [FIX] (PERSISTENCE) Dead code

--- src/main/java/com/excilys/formation/cdb/persistence/AutoRollback.java
+++ src/main/java/com/excilys/formation/cdb/persistence/AutoRollback.java
@@ -24,21 +24,3 @@ public class AutoRollback implements AutoCloseable {
         }
     }
 }
-
-/*public class AutoSetAutoCommit implements AutoCloseable {
-
-    private Connection connection;
-    private boolean originalAutoCommit;
-
-    public AutoSetAutoCommit(Connection connection, boolean autoCommit) throws SQLException {
-        this.connection = connection;
-        originalAutoCommit = connection.getAutoCommit();
-        connection.setAutoCommit(autoCommit);
-    }
-
-    @Override
-    public void close() throws SQLException {
-        connection.setAutoCommit(originalAutoCommit);
-    }
-
-}*/
