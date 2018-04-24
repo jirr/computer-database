a5498935d9bb656735010e9cf02a6cde46160f04
jirr <jirr@excilys.com>
2018-04-24 15:34:16 +0200

    [FEAT] (MultiModule) Maven project split in modules

---
+++ computer-database-service/src/main/java/com/excilys/formation/cdb/service/ComputerService.java
@@ -0,0 +1,117 @@
+package com.excilys.formation.cdb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.excilys.formation.cdb.model.Computer;
+import com.excilys.formation.cdb.persistence.ComputerDAO;
+import com.excilys.formation.cdb.validator.Validator;
+
+/**
+ * @author jirr
+ *
+ */
+@Service
+@EnableTransactionManagement
+public class ComputerService {
+
+    private ComputerDAO computerDAO;
+    private Validator validator;
+
+    @Autowired
+    public ComputerService(ComputerDAO computerDAO, Validator validator) {
+        this.computerDAO = computerDAO;
+        this.validator = validator;
+    }
+
+    /**
+     * @param offset index of the first element
+     * @param numberToDisplay number of object to display
+     * @param keywords The keywords for the search, can be empty
+     * @param sortBy Name of the column to sort on
+     * @param asc Is the sort asc or desc
+     * @return List<Computer> the sublist (page) of computers
+     * @throws ServiceException if failure in persistence execution
+     */
+    public List<Computer> subListComputer(int offset, int numberToDisplay, String keywords, String sortBy, boolean asc) {
+        return computerDAO.subList(offset, numberToDisplay, keywords, sortBy, asc);
+    }
+
+    /**
+     * @param keywords The keywords for the search, can be empty
+     * @return int number of companies
+     */
+    public int countAllComputers(String keywords) {
+        return computerDAO.countAllComputer(keywords);
+    }
+
+    /**
+     * @param id The id of computer
+     * @return Computer with the right id
+     * @throws ServiceException if the id does not exist
+     */
+    public Computer selectOne(int id) throws ServiceException {
+        return validator.computerIdValidation(id);
+    }
+
+    /**
+     * @param computer The computer object to create
+     * @return String validation text
+     * @throws ServiceException if the creation becomes wild
+     */
+    public String createComputer(Computer computer) throws ServiceException {
+        validator.nameValidation(computer.getName());
+        if (computer.getDateIntroduced().isPresent() && computer.getDateDiscontinued().isPresent()) {
+            validator.datesCompatibilityValidation(computer.getDateIntroduced().get(), computer.getDateDiscontinued().get());
+        }
+        if (computer.getDateIntroduced().isPresent()) {
+            validator.dateValidation(computer.getDateIntroduced().get());
+        }
+        if (computer.getDateDiscontinued().isPresent()) {
+            validator.dateValidation(computer.getDateDiscontinued().get());
+        }
+        if (computer.getManufactor().isPresent()) {
+            validator.manufactorValidation(computer.getManufactor().get().getId());
+        }
+        computerDAO.createComputer(computer);
+        return "New computer added to database.";
+    }
+
+    /**
+     * @param computer The computer object to update
+     * @return String validation text
+     * @throws ServiceException if the updating becomes wild
+     */
+    public String updateComputer(Computer computer) throws ServiceException {
+        validator.computerIdValidation(computer.getId());
+        validator.nameValidation(computer.getName());
+        if (computer.getDateIntroduced().isPresent() && computer.getDateDiscontinued().isPresent()) {
+            validator.datesCompatibilityValidation(computer.getDateIntroduced().get(), computer.getDateDiscontinued().get());
+        }
+        if (computer.getManufactor().isPresent()) {
+            validator.manufactorValidation(computer.getManufactor().get().getId());
+        }
+        computerDAO.updateComputer(computer);
+        return "Computer " + computer.getId() + " updated.";
+    }
+
+    /**
+     * @param ids The ids of the computer to delete from the DB
+     * @return String validation test
+     * @throws ServiceException if the deleting becomes wild
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public String deleteComputer(int... ids) throws ServiceException {
+        String strIds = "";
+        for (int id : ids) {
+            validator.computerIdValidation(id);
+            computerDAO.deleteComputer(id);
+            strIds += "(" + id + ")";
+        }
+        return "Computer(s) " + strIds + " removed from database.\n";
+    }
+}
---
+++ computer-database-webapp/src/main/java/com/excilys/formation/cdb/controller/ComputerController.java
@@ -0,0 +1,123 @@
+package com.excilys.formation.cdb.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.excilys.formation.cdb.dto.ComputerDTO;
+import com.excilys.formation.cdb.mapper.ComputerMapper;
+import com.excilys.formation.cdb.service.CompanyService;
+import com.excilys.formation.cdb.service.ComputerService;
+import com.excilys.formation.cdb.service.ServiceException;
+import com.excilys.formation.cdb.pagination.ComputerPage;
+
+@Controller
+@RequestMapping("/computer")
+public class ComputerController {
+
+    private ComputerService computerService;
+    private CompanyService companyService;
+    private ComputerMapper computerMapper;
+
+    public ComputerController(ComputerService computerService, CompanyService companyService, ComputerMapper computerMapper) {
+        this.computerService = computerService;
+        this.companyService = companyService;
+        this.computerMapper = computerMapper;
+    }
+
+    @GetMapping("/dashboard")
+    public ModelAndView doGetDashboard(@RequestParam(value = "search", defaultValue = "") String search,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "index", defaultValue = "1") int index,
+            @RequestParam(value = "sort", defaultValue = "") String sort,
+            @RequestParam(value = "asc", defaultValue = "true") boolean asc) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            ComputerPage page = new ComputerPage(computerService, size, search, index - 1, sort, asc);
+            int nbComputer = computerService.countAllComputers(page.getKeywords());
+            List<ComputerDTO> computersDTO = new ArrayList<>();
+            page.getContent().forEach(computer -> computersDTO.add(computerMapper.computerToDTO(computer)));
+            addAllObject(modelAndView, nbComputer, computersDTO, page);
+        } catch (ServiceException e) {
+            modelAndView.addObject("errorMessage", e.getMessage());
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/dashboard")
+    public ModelAndView doPostDashboard(@RequestParam(value = "selection") int[] ids) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            modelAndView.addObject("successMessage", computerService.deleteComputer(ids));
+        } catch (ServiceException e) {
+            modelAndView.addObject("errorMessage", e.getMessage());
+        }
+        return modelAndView;
+    }
+    
+    @GetMapping("/add")
+    public ModelAndView doGetAddComputer() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("companyList", companyService.listAllCompany());
+        modelAndView.addObject("computer", new ComputerDTO());
+        return modelAndView;
+    }
+
+    @PostMapping("/add")
+    public ModelAndView doPostAddComputer(@ModelAttribute("computer") ComputerDTO computerDTO, Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            String successMessage = computerService.createComputer(computerMapper.dtoToComputer(computerDTO));
+            modelAndView.addObject("successMessage", successMessage);
+        } catch (ServiceException e) {
+            modelAndView.addObject("errorMessage", e.getMessage());
+        }  
+        return modelAndView;
+    }
+    
+    @GetMapping("/edit")
+    public ModelAndView doGetEditComputer(@RequestParam(value = "id") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("companyList", companyService.listAllCompany());
+        try {
+            ComputerDTO computerDTO = computerMapper.computerToDTO(computerService.selectOne(id));
+            modelAndView.addObject("computer", computerDTO);
+        } catch (ServiceException e) {
+            modelAndView.addObject("errorMessage", e.getMessage());
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView doPostEditComputer(@ModelAttribute("computer") ComputerDTO computerDTO, Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            String successMessage = computerService.updateComputer(computerMapper.dtoToComputer(computerDTO));
+            modelAndView.addObject("successMessage", successMessage);
+        } catch (ServiceException e) {
+            modelAndView.addObject("errorMessage", e.getMessage());
+        }
+        return modelAndView;
+    }
+
+    private ModelAndView addAllObject (ModelAndView modelAndView, int nbComputer, List<ComputerDTO> computersDTO, ComputerPage page) {
+        modelAndView.addObject("nbComputers", nbComputer);
+        modelAndView.addObject("computer_list", computersDTO);
+        modelAndView.addObject("keywords", page.getKeywords());
+        modelAndView.addObject("maxIndex", page.getLastPageIndex() + 1);
+        modelAndView.addObject("currentIndex", page.getCurrentPageIndex() + 1);
+        modelAndView.addObject("sortBy", page.getSortBy());
+        modelAndView.addObject("asc", page.isAsc());
+        modelAndView.addObject("size", page.getSize());
+        return modelAndView;
+    }
+
+}
