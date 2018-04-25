eefcc9d5028f46168f6d6a4ee368171a9a970fd5
jirr <jirr@excilys.com>
2018-04-25 17:39:15 +0200

    [WIP] (QueryDSL) CompanyDAO done, ComputerDAO in progress

--- computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/CompanyDAO.java
+++ computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/CompanyDAO.java
@@ -1,85 +1,94 @@
 package com.excilys.formation.cdb.persistence;
 
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.Optional;
 
-import javax.sql.DataSource;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.excilys.formation.cdb.mapper.CompanyMapper;
 import com.excilys.formation.cdb.model.Company;
+import com.excilys.formation.cdb.model.QCompany;
+import com.querydsl.jpa.hibernate.HibernateDeleteClause;
+import com.querydsl.jpa.hibernate.HibernateQuery;
 
-/**
- * @author jirr
- *
- */
 @Repository
 public class CompanyDAO {
 
-    private final String SELECT_ALL_COMPANIES = "SELECT ca.id as caId, ca.name as caName FROM company ca";
-    private final String COUNT_ALL_COMPANIES = "SELECT COUNT(computer.id) FROM computer LEFT JOIN company ON company.id = computer.company_id";
-    private final String SELECT_SOME_COMPANIES = "SELECT ca.id as caId, ca.name as caName FROM company ca LIMIT ? OFFSET ?;";
-    private final String SELECT_ONE_COMPANY = "SELECT ca.id as caId, ca.name as caName FROM company ca WHERE ca.id = ?;";
-    private final String DELETE_ONE_COMPANY = "DELETE FROM computer WHERE id=?;";
-
-    private JdbcTemplate jdbcTemplate;
-    private CompanyMapper companyMapper;
-
-    @Autowired
-    public CompanyDAO(DataSource dataSource, CompanyMapper companyMapper) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.companyMapper = companyMapper;
+    private SessionFactory sessionFactory;
+    private QCompany qCompany;
+    
+    @Autowired 
+    public CompanyDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+        this.qCompany = QCompany.company;
     }
 
     /**
      * @param id of Company that should be in the DB
      * @return Optional<Company> contains the company, could be empty if the id does not exist
-     * @throws DBException if can't reach the database
      */
     public Optional<Company> selectOne(int id) {
-        Company company;
-        try {
-            company = this.jdbcTemplate.query(SELECT_ONE_COMPANY, (ResultSet resultSet, int row) -> companyMapper.resToCompany(resultSet), id).get(0);
-        } catch (IndexOutOfBoundsException e) {
-            company = null;
-        }
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> hibernateQuery = new HibernateQuery<>(session);
+        Company company = hibernateQuery.select(qCompany)
+                                        .from(qCompany)
+                                        .where(qCompany.id.eq(id))
+                                        .fetchOne();
+        session.close();
         return Optional.ofNullable(company);
     }
 
     /**
-     * @return List<Company>
-     * @throws DBException if can't reach the database
+     * @return List<Company> The list of all companies
      */
     public List<Company> list() {
-        return this.jdbcTemplate.query(SELECT_ALL_COMPANIES, (resultSet, row) -> companyMapper.resToCompany(resultSet));
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> hibernateQuery = new HibernateQuery<>(session);
+        List<Company> companyList = hibernateQuery.select(qCompany)
+                                                .from(qCompany)
+                                                .fetch();
+        session.close();
+        return companyList;
     }
-
+    
     /**
      * @return int number of companies
      */
     public int countAllCompany() {
-        return this.jdbcTemplate.queryForObject(COUNT_ALL_COMPANIES, Integer.class);
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> hibernateQuery = new HibernateQuery<>(session);
+        int number = (int) hibernateQuery.select(qCompany.id)
+                                       .from(qCompany)
+                                       .fetchCount();
+        session.close();
+        return number;
     }
 
     /**
      * @param limit index du dernier element
      * @param offset index du premier element
      * @return List<Company> The sublist of Company object from the DB
-     * @throws DBException if can't reach the database
      */
     public List<Company> subList(int offset, int limit) {
-        return this.jdbcTemplate.queryForList(SELECT_SOME_COMPANIES, new Object[] {limit, offset}, Company.class);
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> hibernateQuery = new HibernateQuery<>(session);
+        List<Company> companySubList = hibernateQuery.select(qCompany)
+                                                    .from(qCompany)
+                                                    .offset(offset)
+                                                    .limit(limit)
+                                                    .fetch();
+        session.close();
+        return companySubList;
     }
 
     /**
      * @param id the ID of computer to delete from the DB
-     * @throws DBException if can't reach the database
      */
     public void deleteCompany(int id) {
-        this.jdbcTemplate.update(DELETE_ONE_COMPANY, new Object[] {id});
+        Session session = sessionFactory.openSession();
+        new HibernateDeleteClause(session, qCompany).where(qCompany.id.eq(id)).execute();
+        session.close();
     }
 }
--- computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/ComputerDAO.java
+++ computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/ComputerDAO.java
@@ -1,74 +1,77 @@
 package com.excilys.formation.cdb.persistence;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-import javax.sql.DataSource;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.excilys.formation.cdb.mapper.ComputerMapper;
+import com.excilys.formation.cdb.model.Company;
 import com.excilys.formation.cdb.model.Computer;
+import com.excilys.formation.cdb.model.QCompany;
+import com.excilys.formation.cdb.model.QComputer;
+import com.querydsl.jpa.hibernate.HibernateDeleteClause;
+import com.querydsl.jpa.hibernate.HibernateQuery;
+import com.querydsl.jpa.hibernate.HibernateUpdateClause;
 
 @Repository
 public class ComputerDAO {
 
-    private final String SELECT_ALL_COMPUTERS = "SELECT cu.id as cuId, cu.name as cuName, introduced, discontinued, ca.id as caId, ca.name as caName FROM computer cu LEFT JOIN company ca ON ca.id = cu.company_id";
-    private final String SELECT_ONE_COMPUTER = "SELECT cu.id as cuId, cu.name as cuName, introduced, discontinued, ca.id as caId, ca.name as caName FROM computer cu LEFT JOIN company ca ON ca.id = cu.company_id WHERE cu.id = ?;";
-    private final String CREATE_COMPUTER = "INSERT INTO computer (name, introduced, discontinued, company_id) VALUES (?, ?, ?, ?);";
-    private final String UPDATE_COMPUTER = "UPDATE computer SET name = ?, introduced = ?, discontinued = ?, company_id = ? WHERE computer.id = ?;";
-    private final String DELETE_COMPUTER = "DELETE FROM computer WHERE id=?;";
-    private final String COUNT_ALL_COMPUTERS = "SELECT COUNT(computer.id) FROM computer LEFT JOIN company ON company.id = computer.company_id";
-    private final String SELECT_COMPUTERS_WITH_COMPANY = "SELECT cu.id FROM computer as cu LEFT JOIN company as ca ON cu.id = ca.id WHERE cu.company_id = ?;";
+    private SessionFactory sessionFactory;
+    private QComputer qComputer;
 
-    private JdbcTemplate jdbcTemplate;
-    private ComputerMapper computerMapper;
-
-    @Autowired
-    public ComputerDAO(DataSource dataSource, ComputerMapper computerMapper) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.computerMapper = computerMapper;
+    @Autowired 
+    public ComputerDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+        this.qComputer = QComputer.computer;
     }
 
     /**
      * @return List<Computer> The list of all Computer object from the DB
      */
     public List<Computer> listAll() {
-        return this.jdbcTemplate.queryForList(SELECT_ALL_COMPUTERS, Computer.class);
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Computer> hibernateQuery = new HibernateQuery<>(session);
+        List<Computer> computerList = hibernateQuery.select(qComputer)
+                                                    .from(qComputer)
+                                                    .fetch();
+        session.close();
+        return computerList;
     }
 
     /**
      * @param computer the computer object to create in the DB
      */
     public void createComputer(Computer computer) {
-        this.jdbcTemplate.update(CREATE_COMPUTER, preparedStatement -> {
-                computerToPreparedStatement(computer, preparedStatement);
-            });
+        Session session = sessionFactory.openSession();
+        session.save(computer);
+        session.close();
     }
 
     /**
      * @param computer the computer object to create in the DB
      */
     public void updateComputer(Computer computer) {
-        this.jdbcTemplate.update(UPDATE_COMPUTER, preparedStatement -> {
-                computerToPreparedStatement(computer, preparedStatement);
-                preparedStatement.setInt(5, computer.getId());
-            });
+        Session session = sessionFactory.openSession();
+        new HibernateUpdateClause(session, qComputer).where(qComputer.id.eq(computer.getId()))
+                                 .set(qComputer.name, computer.getName())
+                                 .set(qComputer.dateIntroduced, computer.getDateIntroduced())
+                                 .set(qComputer.dateDiscontinued, computer.getDateDiscontinued())
+                                 .set(qComputer.manufactor, computer.getManufactor())).execute();
+        session.close();
     }
 
     /**
      * @param ids the IDs of computer to delete from the DB
      */
     public void deleteComputer(int... ids) {
+        Session session = sessionFactory.openSession();
         for (int id : ids) {
-            this.jdbcTemplate.update(DELETE_COMPUTER, id);
+            new HibernateDeleteClause(session, qComputer).where(qComputer.id.eq(id)).execute();
         }
+        session.close();
     }
 
     /**
@@ -76,12 +79,13 @@ public class ComputerDAO {
      * @return Optional<Computer> contains the Computer, could be empty if the id does not exist
      */
     public Optional<Computer> selectOne(int id) {
-        Computer computer;
-        try {
-            computer = this.jdbcTemplate.query(SELECT_ONE_COMPUTER, (ResultSet resultSet, int row) -> computerMapper.resToComputer(resultSet), id).get(0);
-        } catch (IndexOutOfBoundsException e) {
-            computer = null;
-        }
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Computer> hibernateQuery = new HibernateQuery<>(session);
+        Computer computer = hibernateQuery.select(qComputer)
+                                        .from(qComputer)
+                                        .where(qComputer.id.eq(id))
+                                        .fetchOne();
+        session.close();
         return Optional.ofNullable(computer);
     }
 
@@ -90,7 +94,13 @@ public class ComputerDAO {
      * @return Optional<Computer> contains the Computer, could be empty if the id does not exist
      */
     public List<Integer> getComputersWithCompanyId(int id) {
-        List<Integer> ids = this.jdbcTemplate.queryForList(SELECT_COMPUTERS_WITH_COMPANY, int.class);
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Integer> hibernateQuery = new HibernateQuery<>(session);
+        List<Integer> ids = hibernateQuery.select(qComputer.id)
+                                        .from(qComputer)
+                                        .where(qComputer.manufactor.id.eq(id))
+                                        .fetch();
+        session.close();
         return ids;
     }
 
@@ -99,9 +109,14 @@ public class ComputerDAO {
      * @return int number of computers
      */
     public int countAllComputer(String keywords) {
-        String like = (keywords.length() > 0) ? " WHERE computer.name LIKE '%" + keywords + "%' OR company.name LIKE '%" + keywords + "%'" : "";
-        String count = COUNT_ALL_COMPUTERS + like + ";";
-        return this.jdbcTemplate.queryForObject(count, Integer.class);
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Integer> hibernateQuery = new HibernateQuery<>(session);
+        int number = (int) hibernateQuery.select(qComputer.id)
+                                       .from(qComputer)
+                                       .where(qComputer.name.like(keywords).or(qComputer.manufactor.name.like(keywords)))
+                                       .fetchCount();
+        session.close();
+        return number;
     }
 
     /**
@@ -128,31 +143,5 @@ public class ComputerDAO {
         }
         String request = SELECT_ALL_COMPUTERS + like + sort + " LIMIT ? OFFSET ?;";
         return this.jdbcTemplate.query(request, object, (ResultSet resultSet, int row) -> computerMapper.resToComputer(resultSet));
-    }
-
-    /**
-     * @param computer The computer to put in the PreparedStatement
-     * @param preparedStatement The PreparedStatement to fill
-     * @return PreparedStatement The PreparedStatement filled with computer's field
-     * @throws SQLException if can't fill the PreparedStatement
-     */
-    public PreparedStatement computerToPreparedStatement(Computer computer, PreparedStatement preparedStatement) throws SQLException {
-        preparedStatement.setString(1, computer.getName());
-        if (computer.getDateIntroduced().isPresent()) {
-            preparedStatement.setDate(2, Date.valueOf(computer.getDateIntroduced().get()));
-        } else {
-            preparedStatement.setNull(2, java.sql.Types.DATE);
-        }
-        if (computer.getDateDiscontinued().isPresent()) {
-            preparedStatement.setDate(3, Date.valueOf(computer.getDateDiscontinued().get()));
-        } else {
-            preparedStatement.setNull(3, java.sql.Types.DATE);
-        }
-        if (computer.getManufactor().isPresent()) {
-            preparedStatement.setInt(4, computer.getManufactor().get().getId());
-        } else {
-            preparedStatement.setNull(4, java.sql.Types.INTEGER);
-        }
-        return preparedStatement;
     }
 }
