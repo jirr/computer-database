538cb028ac98c2ff9c35cb04336283a38b68ff10
jirr <jirr@excilys.com>
2018-05-03 17:46:51 +0200

    [FEAT] (WebApp) Custom error pages

--- computer-database-webapp/src/main/java/com/excilys/formation/cdb/config/WebInitializer.java
+++ computer-database-webapp/src/main/java/com/excilys/formation/cdb/config/WebInitializer.java
@@ -1,5 +1,8 @@
 package com.excilys.formation.cdb.config;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,5 +20,12 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    protected FrameworkServlet createDispatcherServlet(WebApplicationContext wac) {
+        DispatcherServlet ds = new DispatcherServlet(wac);
+        ds.setThrowExceptionIfNoHandlerFound(true);
+        return ds;
     }
 }
